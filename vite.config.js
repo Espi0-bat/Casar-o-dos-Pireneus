6cca5eabec15e3aeef665a4567eae3c50712d141
Ezer <moutinhoezer@gmail.com>
2026-04-24 15:14:30 -0300

    dada

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Casar-o-dos-Pireneus/',
+  base: '/Casa_dos_Pireneus/',
   build: {
     minify: 'terser',
     terserOptions: {
